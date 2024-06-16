6379b9e554989149f6526c5a19c46b64a621b518
Sariel Goldvarg <sariel.goldvarg@flex.com>
2024-06-16 17:39:11 +0300

    Added the Option to Manage User Roles
    User can now add and remove hes user roles.

---
+++ TRMDataManager/Models/UserRolePairModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TRMDataManager.Models
+{
+    public class UserRolePairModel
+    {
+        public string userId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
--- TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.ComponentModel;
 using System.Dynamic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using TRMDesktopUI.Library.Api;
@@ -15,6 +16,12 @@ namespace TRMDesktopUI.ViewModels
         private readonly IWindowManager _window;
         private readonly IUserEndpoint _userEndpoint;
         private BindingList<UserModel> _users;
+        private UserModel _selectedUser;
+        private string _selectedUserName;
+        private BindingList<string> _userRoles = new BindingList<string>();
+        private BindingList<string> _availableRoles = new BindingList<string>();
+        private string _selectedUserRole;
+        private string _selectedAvailableRole;
 
         public BindingList<UserModel> Users
         {
@@ -28,6 +35,89 @@ namespace TRMDesktopUI.ViewModels
                 NotifyOfPropertyChange(() => Users);
             }
         }
+
+        public UserModel SelectedUser
+        {
+            get
+            {
+                return _selectedUser;
+            }
+            set
+            {
+                _selectedUser = value;
+                SelectedUserName = value.Email;
+                UserRoles.Clear();
+                UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+                LoadRoles();
+                NotifyOfPropertyChange(() => SelectedUser);
+            }
+        }
+
+        public string SelectedUserRole
+        {
+            get
+            {
+                return _selectedUserRole;
+            }
+            set
+            {
+                _selectedUserRole = value;
+                NotifyOfPropertyChange(() => SelectedUserRole);
+            }
+        }
+
+        public string SelectedAvailableRole
+        {
+            get
+            {
+                return _selectedAvailableRole;
+            }
+            set
+            {
+                _selectedAvailableRole = value;
+                NotifyOfPropertyChange(() => SelectedAvailableRole);
+            }
+        }
+
+        public string SelectedUserName
+        {
+            get
+            {
+                return _selectedUserName;
+            }
+            set
+            {
+                _selectedUserName = value;
+                NotifyOfPropertyChange(() => SelectedUserName);
+            }
+        }
+
+        public BindingList<string> UserRoles
+        {
+            get
+            {
+                return _userRoles;
+            }
+            set
+            {
+                _userRoles = value;
+                NotifyOfPropertyChange(() => UserRoles);
+            }
+        }
+
+        public BindingList<string> AvailableRoles
+        {
+            get
+            {
+                return _availableRoles;
+            }
+            set
+            {
+                _availableRoles = value;
+                NotifyOfPropertyChange(() => AvailableRoles);
+            }
+        }
+
 
         public UserDisplayViewModel(StatusInfoViewModel status, IWindowManager window, IUserEndpoint userEndpoint)
         {
@@ -69,6 +159,35 @@ namespace TRMDesktopUI.ViewModels
         {
             var userList = await _userEndpoint.GetAll();
             Users = new BindingList<UserModel>(userList);
+        }
+
+        private async Task LoadRoles()
+        {
+            var roles = await _userEndpoint.GetAllRoles();
+
+            foreach (var role in roles)
+            {
+                if (UserRoles.IndexOf(role.Value) < 0)
+                {
+                    AvailableRoles.Add(role.Value);
+                }
+            }
+        }
+
+        public async Task AddSelectedRole()
+        {
+            await _userEndpoint.AddUserToRole(SelectedUser.Id, SelectedAvailableRole);
+
+            UserRoles.Add(SelectedAvailableRole);
+            AvailableRoles.Remove(SelectedAvailableRole);
+        }
+
+        public async Task RemoveSelectedRole()
+        {
+            await _userEndpoint.RemoveUserFromRole(SelectedUser.Id, SelectedUserRole);
+
+            AvailableRoles.Add(SelectedUserRole);
+            UserRoles.Remove(SelectedUserRole);
         }
     }
 }
