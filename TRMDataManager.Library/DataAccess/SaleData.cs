22ea6b34952f0b2c4cfb75334ca1432597fa4333
Sariel Goldvarg <sariel.goldvarg@flex.com>
2024-06-16 10:24:51 +0300

    Fix Transaction Exception Bug
    Transactions that were explicitly closed would throw an exception because the transaction was closed twice.

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -72,7 +72,7 @@ namespace TRMDataManager.Library.DataAccess
                         sql.SaveDataInTransaction("dbo.spSaleDetail_Insert", item);
                     }
 
-                    //sql.CommitTransaction();
+                    sql.CommitTransaction();
                 }
                 catch
                 {
